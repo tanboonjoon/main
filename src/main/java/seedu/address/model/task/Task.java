de9cccba2f87f3bcee383ae9c155647c2e93bd88
SAN SOK SAN <sansoksan@u.nus.edu>
2016-10-06 15:01:46 +0800

    empty commit for travis.

--- src/main/java/seedu/address/model/task/Task.java
+++ src/main/java/seedu/address/model/task/Task.java
@@ -19,7 +19,7 @@ public class Task implements ReadOnlyTask {
     private UniqueTagList tags;
 
     /**
-     * Every field must be present and not null.
+     * Every field must be present and not null. 
      */
     public Task(Name name, Phone phone, Email email, Address address, UniqueTagList tags) {
         assert !CollectionUtil.isAnyNull(name, phone, email, address, tags);
