e68de17c960b8c867ee0801dd7ceb2d5f31a7ae4
MightyCupcakes <gohydj2003@gmail.com>
2016-10-08 20:20:48 +0800

    Modified the Date objects to LocalDateTime

--- src/main/java/seedu/address/model/task/Deadline.java
+++ src/main/java/seedu/address/model/task/Deadline.java
@@ -1,22 +1,22 @@
 package seedu.address.model.task;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 import seedu.address.model.tag.UniqueTagList;
 /**
-* A deadline is a task that has only a end date  
+* A deadline is a task that has only a ending datetime
 *
  */
 public class Deadline extends Task {
 	
-	private final Date endDate;
+	private final LocalDateTime endDate;
 
-	public Deadline(String name, String description, Date endDate, UniqueTagList tags) {
+	public Deadline(String name, String description, LocalDateTime endDate, UniqueTagList tags) {
 		super(name,description,tags);
 		this.endDate = endDate;
 	}
 	
-	public Date getEndDate() {
+	public LocalDateTime getEndDate() {
 		return endDate;
 	}
 	
