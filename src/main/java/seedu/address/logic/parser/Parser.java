8481d65785b756ce5ebdd831a137492517c94363
Ning Yu <ningyu321@gmail.com>
2016-10-11 21:40:06 +0800

    implemented a preparer abstract class

--- src/main/java/seedu/address/logic/parser/Parser.java
+++ src/main/java/seedu/address/logic/parser/Parser.java
@@ -28,6 +28,8 @@ import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.IncorrectCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.preparer.Preparer;
+import seedu.address.logic.preparer.addPreparer;
 
 /**
  * Parses user input.
@@ -38,8 +40,6 @@ public class Parser {
      * Used for initial separation of command word and args.
      */
     private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
-
-    private static final Pattern TASK_INDEX_ARGS_FORMAT = Pattern.compile("(?<targetIndex>.+)");
 
     private static final Pattern KEYWORDS_ARGS_FORMAT =
             Pattern.compile("(?<keywords>\\S+(?:\\s+\\S+)*)"); // one or more keywords separated by whitespace
@@ -62,25 +62,27 @@ public class Parser {
         if (!matcher.matches()) {
             return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
         }
-
+        
+        Preparer preparer = new Preparer(); // abstract class can't be created
+        
         final String commandWord = matcher.group("commandWord");
         final String arguments = matcher.group("arguments");
         switch (commandWord) {
 
         case AddCommand.COMMAND_WORD:
-            return prepareAdd(arguments);
+            return new addPreparer().prepare(arguments);
 
         case SelectCommand.COMMAND_WORD:
-            return prepareSelect(arguments);
+            return preparer.prepareSelect(arguments);
 
         case DeleteCommand.COMMAND_WORD:
-            return prepareDelete(arguments);
+            return preparer.prepareDelete(arguments);
 
         case ClearCommand.COMMAND_WORD:
             return new ClearCommand();
 
         case FindCommand.COMMAND_WORD:
-            return prepareFind(arguments);
+            return preparer.prepareFind(arguments);
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
@@ -94,42 +96,6 @@ public class Parser {
         default:
             return new IncorrectCommand(MESSAGE_UNKNOWN_COMMAND);
         }
-    }
-
-    /**
-     * Parses arguments in the context of the add task command.
-     *
-     * @param args full command args string
-     * @return the prepared command
-     */
-    private Command prepareAdd(String args){
-    	
-    	ArgumentsParser parser = new ArgumentsParser() ;
-    	
-    	parser
-    		.addNoFlagArg(CommandArgs.NAME)
-    		.addOptionalArg(CommandArgs.DESC)
-    		.addOptionalArg(CommandArgs.TAGS) ;
-    	
-    	try {
-    		parser.parse(args);
-    	} catch (IncorrectCommandException e) {
-    		 return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
-    	}
-    	
-    	try {
-    		return new AddCommand(
-    				parser.getArgValue(CommandArgs.NAME).get(),
-    				parser.getArgValue(CommandArgs.DESC).isPresent() ? parser.getArgValue(CommandArgs.DESC).get() : "",
-    				parser.getArgValue(CommandArgs.START_DATETIME).isPresent() ? parser.getArgValue(CommandArgs.START_DATETIME).get() : null,
-    				parser.getArgValue(CommandArgs.END_DATETIME).isPresent() ? parser.getArgValue(CommandArgs.END_DATETIME).get() : null,
-    				parser.getArgValues(CommandArgs.TAGS).isPresent() ? Sets.newHashSet(parser.getArgValues(CommandArgs.TAGS).get()) : Collections.emptySet()
-    		) ;
-    	} catch (IllegalValueException e) {
-    		 return new IncorrectCommand(e.getMessage());
-    	} catch (DateTimeParseException e) {
-    		return new IncorrectCommand(String.format(MESSAGE_INVALID_DATE_FORMAT, AddCommand.MESSAGE_USAGE));
-    	}
     }
 
     /**
@@ -162,79 +128,5 @@ public class Parser {
     	return (result.matches("^ +$")) ? " " : result ;
     }
 
-    /**
-     * Parses arguments in the context of the delete task command.
-     *
-     * @param args full command args string
-     * @return the prepared command
-     */
-    private Command prepareDelete(String args) {
-
-        Optional<Integer> index = parseIndex(args);
-        if(!index.isPresent()){
-            return new IncorrectCommand(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
-        }
-
-        return new DeleteCommand(index.get());
-    }
-
-    /**
-     * Parses arguments in the context of the select task command.
-     *
-     * @param args full command args string
-     * @return the prepared command
-     */
-    private Command prepareSelect(String args) {
-        Optional<Integer> index = parseIndex(args);
-        if(!index.isPresent()){
-            return new IncorrectCommand(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
-        }
-
-        return new SelectCommand(index.get());
-    }
-
-    /**
-     * Returns the specified index in the {@code command} IF a positive unsigned integer is given as the index.
-     *   Returns an {@code Optional.empty()} otherwise.
-     */
-    private Optional<Integer> parseIndex(String command) {
-        final Matcher matcher = TASK_INDEX_ARGS_FORMAT.matcher(command.trim());
-        if (!matcher.matches()) {
-            return Optional.empty();
-        }
-
-        String index = matcher.group("targetIndex");
-        if(!StringUtil.isUnsignedInteger(index)){
-            return Optional.empty();
-        }
-        return Optional.of(Integer.parseInt(index));
-
-    }
-
-    /**
-     * Parses arguments in the context of the find task command.
-     *
-     * @param args full command args string
-     * @return the prepared command
-     */
-    private Command prepareFind(String args) {
-    	ArgumentsParser parser = new ArgumentsParser() ;
-    	
-    	parser.addNoFlagArg(CommandArgs.NAME) ;
-    	
-    	try {
-    		parser.parse(args);
-    	} catch (IncorrectCommandException e) {
-    		 return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                     FindCommand.MESSAGE_USAGE));
-    	}
-
-        // keywords delimited by whitespace
-        final String[] keywords = parser.getArgValue(CommandArgs.NAME).get().split("\\s+");
-        final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
-        return new FindCommand(keywordSet);
-    }
 
 }
---
+++ src/main/java/seedu/address/logic/preparer/addPreparer.java
@@ -0,0 +1,58 @@
+package seedu.address.logic.preparer;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_DATE_FORMAT;
+
+import java.time.format.DateTimeParseException;
+import java.util.Collections;
+
+import com.google.common.collect.Sets;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.commons.exceptions.IncorrectCommandException;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.IncorrectCommand;
+import seedu.address.logic.parser.ArgumentsParser;
+import seedu.address.logic.parser.CommandArgs;
+
+public class addPreparer extends Preparer {
+    
+    @Override
+    /**
+     * Parses arguments in the context of the add task command.
+     *
+     * @param args full command args string
+     * @return the prepared command
+     */
+    public Command prepare(String args){
+        
+        ArgumentsParser parser = new ArgumentsParser() ;
+        
+        parser
+            .addNoFlagArg(CommandArgs.NAME)
+            .addOptionalArg(CommandArgs.DESC)
+            .addOptionalArg(CommandArgs.TAGS) ;
+        
+        try {
+            parser.parse(args);
+        } catch (IncorrectCommandException e) {
+             return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+        
+        try {
+            return new AddCommand(
+                    parser.getArgValue(CommandArgs.NAME).get(),
+                    parser.getArgValue(CommandArgs.DESC).isPresent() ? parser.getArgValue(CommandArgs.DESC).get() : "",
+                    parser.getArgValue(CommandArgs.START_DATETIME).isPresent() ? parser.getArgValue(CommandArgs.START_DATETIME).get() : null,
+                    parser.getArgValue(CommandArgs.END_DATETIME).isPresent() ? parser.getArgValue(CommandArgs.END_DATETIME).get() : null,
+                    parser.getArgValues(CommandArgs.TAGS).isPresent() ? Sets.newHashSet(parser.getArgValues(CommandArgs.TAGS).get()) : Collections.emptySet()
+            ) ;
+        } catch (IllegalValueException e) {
+             return new IncorrectCommand(e.getMessage());
+        } catch (DateTimeParseException e) {
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_DATE_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+    }
+
+}
