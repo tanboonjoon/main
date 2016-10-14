4b968c0e375cd03ea60746076018788bd899d9a8
SAN SOK SAN <sansoksan@u.nus.edu>
2016-10-14 20:04:00 +0800

    Clean up and revert Command class.

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -1,27 +1,16 @@
 package seedu.address.logic.commands;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.events.ui.IncorrectCommandAttemptedEvent;
 import seedu.address.model.Model;
-import seedu.address.model.task.ReadOnlyTask;
 
 /**
  * Represents a command with hidden internal logic and the ability to be executed.
  */
-public abstract class Command implements Undoable {
+public abstract class Command {
     protected Model model;
-    protected Map<String, LinkedList<ReadOnlyTask>> taskHistory = new LinkedHashMap<String, LinkedList<ReadOnlyTask>>();
-    protected List<String> commandHistory = new LinkedList<String>();
     
     /**
      * Constructs a feedback message to summarise an operation that displayed a listing of task.
