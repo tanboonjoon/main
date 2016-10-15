8d301b9a363f16d8b507ff0b50ffad79a511c1f9
MightyCupcakes <gohydj2003@gmail.com>
2016-10-15 12:21:37 +0800

    Fixed indentations

--- src/main/java/seedu/address/logic/parser/CommandArgs.java
+++ src/main/java/seedu/address/logic/parser/CommandArgs.java
@@ -1,31 +1,31 @@
 package seedu.address.logic.parser;
 
 public enum CommandArgs {
-	NAME(""),
-	DESC("d/"),
-	TAGS("t/"),
-	START_DATETIME ("st/"),
-	END_DATETIME ("et/"),
+    NAME(""),
+    DESC("d/"),
+    TAGS("t/"),
+    START_DATETIME ("st/"),
+    END_DATETIME ("et/"),
 
-	//Arguments for find command to parse
-	FIND_DAY ("day/"),
-	FIND_WEEK ("week/"),
-	FIND_ALL ("all/"),
-    
+    //Arguments for find command to parse
+    FIND_DAY ("day/"),
+    FIND_WEEK ("week/"),
+    FIND_ALL ("all/"),
+
     // Special NULL flag to indicate useless arguments
     NULL_ARG("^/")
-    
-    ;
-	
-	
-	private String commandString ;
 
-	private CommandArgs (String cmd) {
-		commandString = cmd ;
-	}
-	
-	@Override
-	public String toString() {
-		return commandString ;
-	}
+    ;
+
+
+    private String commandString ;
+
+    private CommandArgs (String cmd) {
+        commandString = cmd ;
+    }
+
+    @Override
+    public String toString() {
+        return commandString ;
+    }
 }
