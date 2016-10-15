b4528ffbf3176a74847f47786ca0c67a09e033c9
Ning Yu <ningyu321@gmail.com>
2016-10-15 15:05:14 +0800

    added mark done to UI

---
+++ src/main/java/seedu/address/logic/commands/MarkCommand.java
@@ -0,0 +1,46 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.UnmodifiableObservableList;
+import seedu.address.model.task.ReadOnlyTask;
+import seedu.address.model.task.UniqueTaskList.TaskNotFoundException;
+
+public class MarkCommand extends Command {
+
+	public static final String COMMAND_WORD = "mark";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Mark the task identified by the index number used in the last task listing as done.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_MARK_TASK_SUCCESS = "Marked Task %1$s as done";
+
+    public final int targetIndex;
+
+    public MarkCommand(int targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute() {
+
+        UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
+
+        if (lastShownList.size() < targetIndex) {
+            indicateAttemptToExecuteIncorrectCommand();
+            return new CommandResult(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        }
+
+        ReadOnlyTask taskToMark = lastShownList.get(targetIndex - 1);
+
+        try {
+            model.markTask(taskToMark);
+        } catch (TaskNotFoundException pnfe) {
+            assert false : "The target task cannot be missing";
+        }
+
+        return new CommandResult(String.format(MESSAGE_MARK_TASK_SUCCESS, taskToMark));
+    }
+
+}
---
+++ src/main/java/seedu/address/logic/parser/MarkCommandParser.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Optional;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.MarkCommand;
+import seedu.address.logic.commands.IncorrectCommand;
+
+public class MarkCommandParser extends CommandParser {
+    /**
+     * Parses arguments in the context of the mark task as done command.
+     *
+     * @param args full command args string
+     * @return the prepared command
+     */
+    
+    @Override
+    public Command prepareCommand(String args) {
+        Optional<Integer> index = parseIndex(args);
+        if(!index.isPresent()){
+            return new IncorrectCommand(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE));
+        }
+
+        return new MarkCommand(index.get());
+    }
+}
