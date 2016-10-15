32d21bc78f32120d36fe6d5c6f988e146d5cdea0
tanboonjoon <tbj_999@hotmail.com>
2016-10-15 12:55:14 +0800

    remove regex parsing

--- src/main/java/seedu/address/logic/commands/CdCommand.java
+++ src/main/java/seedu/address/logic/commands/CdCommand.java
@@ -1,4 +1,14 @@
 package seedu.address.logic.commands;
+
+import java.io.File;
+import java.io.IOException;
+
+import seedu.address.commons.core.Config;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.storage.Storage;
+import seedu.address.storage.StorageManager;
+import seedu.address.storage.XmlTaskForceStorage;
+
 /*
  * 
  * This command save the storage file into a different location
@@ -9,18 +19,39 @@ public class CdCommand extends Command{
 	public final static String COMMAND_WORD = "cd";
 	public final static String MESSAGE_USAGE = COMMAND_WORD + ": change the filestorage saving location. \n"
 			+ "Parameters : FILEPATH (must be a valid path) \n"
-			+ "Example : cd /destkop/important_folder";
+			+ "Example : cd destkop/important_folder/saveData.xml";
+	public final static String MESSAGE_SUCCESS = "file has been saved to the location successfully.";
+	public final static String MESSAGE_FAILURE = "please enter a valid filepath";
 	
-
-	private String filepath;
+	private final String newStoragePath;
+	private final String originalStoragePath;
+	private Config config;
+	private Storage storage;
+	private XmlTaskForceStorage xmlStorageRetriever;
 	
-	public CdCommand(String filepath) {
-		this.filepath = filepath;
+	public CdCommand(String filepath) throws IllegalValueException{
+		config = new Config();
+		originalStoragePath = config.getTaskForceFilePath();
+		this.newStoragePath = filepath;
+		xmlStorageRetriever = new XmlTaskForceStorage(newStoragePath);
 	}
+
+
+
 	@Override
-	public CommandResult execute() {
+	public CommandResult execute()  {
 		// TODO Auto-generated method stub
-		return null;
+
+
+		
+		return new CommandResult(MESSAGE_SUCCESS);
+	}
+	
+	public boolean isValidFilePath(String filepath) {
+		File file = new File(filepath);
+		System.out.println(file.exists());
+		System.out.println(file.isDirectory());
+		return file.isDirectory();
 	}
 
 }
--- src/main/java/seedu/address/logic/parser/CdCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CdCommandParser.java
@@ -2,6 +2,10 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.exceptions.IncorrectCommandException;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.CdCommand;
@@ -12,20 +16,17 @@ public class CdCommandParser extends CommandParser{
 
 	@Override
 	public Command prepareCommand(String args) {
-		// TODO Auto-generated method stub
-		ArgumentsParser parser = new ArgumentsParser() ;
-		
-		parser
-		.addNoFlagArg(CommandArgs.NAME);
 
+		
+		try {
+			return new CdCommand (args.trim());
+					
+		} catch (IllegalValueException e) {
+			return new IncorrectCommand(e.getMessage());
+		}
 
 	
-	try {
-		parser.parse(args);
-	} catch (IncorrectCommandException e) {
-		return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, CdCommand.MESSAGE_USAGE));
 	}
-	return null;
-	}
+
 }
 
