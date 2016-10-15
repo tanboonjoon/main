43eca694e292bab40b6d9afdc24b0fd51638f04a
SAN SOK SAN <sansoksan@u.nus.edu>
2016-10-15 11:33:57 +0800

    Hold back MESSAGE_USAGE first. Find a way to test it later.

--- src/main/java/seedu/address/logic/parser/UndoCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UndoCommandParser.java
@@ -13,11 +13,11 @@ public class UndoCommandParser extends CommandParser {
     public Command prepareCommand(String args) {
         ArgumentsParser parser = new ArgumentsParser();
                 
-        try {
-            parser.parse(args);
-        } catch (IncorrectCommandException e) {
-            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, UndoCommand.MESSAGE_USAGE));
-        }
+//        try {
+//            parser.parse(args);
+//        } catch (IncorrectCommandException e) {
+//            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, UndoCommand.MESSAGE_USAGE));
+//        }
         
         return new UndoCommand();
     }
