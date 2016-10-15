f80361d5302776a4a8b7a5fb933d8168267a546e
tanboonjoon <tbj_999@hotmail.com>
2016-10-15 13:49:32 +0800

    modify stroageManager to update location

--- src/main/java/seedu/address/logic/commands/CdCommand.java
+++ src/main/java/seedu/address/logic/commands/CdCommand.java
@@ -2,6 +2,9 @@ package seedu.address.logic.commands;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import seedu.address.commons.core.Config;
 import seedu.address.commons.exceptions.IllegalValueException;
@@ -20,18 +23,21 @@ public class CdCommand extends Command{
 	public final static String MESSAGE_USAGE = COMMAND_WORD + ": change the filestorage saving location. \n"
 			+ "Parameters : FILEPATH (must be a valid path) \n"
 			+ "Example : cd destkop/important_folder/saveData.xml";
-	public final static String MESSAGE_SUCCESS = "file has been saved to the location successfully.";
+	public final static String MESSAGE_SUCCESS = "file has been saved to the location successfully in ";
 	public final static String MESSAGE_FAILURE = "please enter a valid filepath";
 	
 	private final String newStoragePath;
 	private final String originalStoragePath;
+	private final String originalJsonPath;
 	private Config config;
 	private Storage storage;
+	private StorageManager storageManager;
 	private XmlTaskForceStorage xmlStorageRetriever;
 	
 	public CdCommand(String filepath) throws IllegalValueException{
 		config = new Config();
 		originalStoragePath = config.getTaskForceFilePath();
+		originalJsonPath = config.getUserPrefsFilePath();
 		this.newStoragePath = filepath;
 		xmlStorageRetriever = new XmlTaskForceStorage(newStoragePath);
 	}
@@ -41,17 +47,31 @@ public class CdCommand extends Command{
 	@Override
 	public CommandResult execute()  {
 		// TODO Auto-generated method stub
-
+		try {
+			xmlStorageRetriever.saveTaskForce(model.getTaskForce());
+			if	(!checkFileLocation(this.newStoragePath)) {
+				return new CommandResult(MESSAGE_FAILURE);
+			}
+			
+			config.setTaskForceFilePath(this.newStoragePath);
+			storageManager.setTaskForceDirectory(this.newStoragePath);
+			storageManager.updateStorageDirectory(config);
+			
+			return new CommandResult(("MESSAGE_SUCCESS " + newStoragePath));
+			
+		}catch (IOException e) {
+			return new CommandResult(MESSAGE_FAILURE);
+		}
 
 		
-		return new CommandResult(MESSAGE_SUCCESS);
+	
 	}
 	
-	public boolean isValidFilePath(String filepath) {
-		File file = new File(filepath);
-		System.out.println(file.exists());
-		System.out.println(file.isDirectory());
-		return file.isDirectory();
+
+	public boolean checkFileLocation(String filepath) {
+		Path path = Paths.get(filepath);
+		return Files.exists(path);
+		
 	}
 
 }
