7957e9689e287a2e703af53330d986e66d76c2cb
MightyCupcakes <gohydj2003@gmail.com>
2016-10-15 12:11:26 +0800

    The argument parser now allows null values for no flag arguments by default (unless specified otherwise).

--- src/main/java/seedu/address/logic/parser/CommandArgs.java
+++ src/main/java/seedu/address/logic/parser/CommandArgs.java
@@ -7,12 +7,15 @@ public enum CommandArgs {
 	START_DATETIME ("st/"),
 	END_DATETIME ("et/"),
 
-
 	//Arguments for find command to parse
 	FIND_DAY ("day/"),
 	FIND_WEEK ("week/"),
-	FIND_ALL ("all/");
-
+	FIND_ALL ("all/"),
+    
+    // Special NULL flag to indicate useless arguments
+    NULL_ARG("^/")
+    
+    ;
 	
 	
 	private String commandString ;
