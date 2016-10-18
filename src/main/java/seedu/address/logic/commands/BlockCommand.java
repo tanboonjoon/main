7de9efb052be6f08d33411db18afd0163bef4a50
MightyCupcakes <gohydj2003@gmail.com>
2016-10-19 00:21:59 +0800

    more block command implementations

--- src/main/java/seedu/address/logic/commands/BlockCommand.java
+++ src/main/java/seedu/address/logic/commands/BlockCommand.java
@@ -1,5 +1,8 @@
 package seedu.address.logic.commands;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class BlockCommand extends Command {
     
     public static final String[] COMMAND_WORD = {
@@ -8,12 +11,17 @@ public class BlockCommand extends Command {
     };
     
     public static final String DEFAULT_COMMAND_WORD = COMMAND_WORD[0] ;
-    public static final String MESSAGE_SUCCESS = "New task added: %1$s";
+    public static final String MESSAGE_SUCCESS = "The following timeslots are reserved for %1$s: %2$s";
     
     public static final String MESSAGE_USAGE = DEFAULT_COMMAND_WORD + ": Reserves timeslots for an event that is unconfirmed. \n" 
             + "Format: Block EVENT_NAME st/START TIME et/END TIME [st/START TIME et/END_TIME]..."
             + "Example: block meeting with boss st/today 2pm et/today 4pm st/tommorrow 2pm et/tommorrow 4pm" ;
-
+    
+    
+    public BlockCommand (String name, List<LocalDateTime> startDates, List<LocalDateTime> endDates) {
+        
+    }
+    
     @Override
     public CommandResult execute() {
         return null ;
