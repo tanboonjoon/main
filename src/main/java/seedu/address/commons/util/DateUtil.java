189d30b5e1ea2d7e116aeae64a5df66efde4dfef
MightyCupcakes <gohydj2003@gmail.com>
2016-10-21 15:23:30 +0800

    Fixed indentation

--- src/main/java/seedu/address/commons/util/DateUtil.java
+++ src/main/java/seedu/address/commons/util/DateUtil.java
@@ -15,85 +15,85 @@ import javafx.util.Pair;
 
 // @@author A0135768R
 public final class DateUtil {
-	
-	public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm a");
-	public static final LocalDateTime END_OF_TODAY = parseStringIntoDateTime("today 2359").get() ;
-	public static final LocalDateTime NOW = LocalDateTime.now() ;
-	
-	public static Optional<LocalDateTime> parseStringIntoDateTime (String rawString) {
-	    
-	    if (rawString == null) {
-	        return Optional.empty() ;
-	    }
-	    
-		Parser dateParser = new Parser() ;
-		
-		List<DateGroup> dates = dateParser.parse(rawString) ;
-		
-		try {
-	        Date date = dates.get(0).getDates().get(0) ;
-	        
-	        return Optional.of(LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault())) ;
-	        
-		} catch (IndexOutOfBoundsException e) {
-		    return Optional.empty() ;
-		}
-	}
-	
-	public static String parseLocalDateTimeIntoString (LocalDateTime datetime) {
-	    assert datetime != null ;
-	    
-		return datetime.format(FORMATTER) ;
-	}
-	
-	
-	private static Optional<Pair<LocalDateTime, LocalDateTime>> determineStartAndEndDateTime(Optional<LocalDateTime> startDate, Optional<LocalDateTime> endDate) {
-        
-	    LocalDateTime computedStartDate ;
-	    LocalDateTime computedEndDate ;
-	    
-	    if (!startDate.isPresent() && !endDate.isPresent()) {
-	        computedStartDate = DateUtil.NOW ;
-	        computedEndDate = DateUtil.END_OF_TODAY ;
-	        
-	        return Optional.of(new Pair<LocalDateTime, LocalDateTime> (computedStartDate, computedEndDate)) ;
-	    }
-	    
-	    if (!startDate.isPresent() && endDate.isPresent()) {
-	        computedStartDate = DateUtil.NOW ;
-            computedEndDate = endDate.get() ;
-            
-            return Optional.of(new Pair<LocalDateTime, LocalDateTime> (computedStartDate, computedEndDate)) ;
-	    }
-	    
-	    if (startDate.isPresent() && !endDate.isPresent()) {
-            computedStartDate = startDate.get() ;
-            computedEndDate = startDate.get().withHour(23).withMinute(59) ;
-            
+
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm a");
+    public static final LocalDateTime END_OF_TODAY = parseStringIntoDateTime("today 2359").get() ;
+    public static final LocalDateTime NOW = LocalDateTime.now() ;
+
+    public static Optional<LocalDateTime> parseStringIntoDateTime (String rawString) {
+
+        if (rawString == null) {
+            return Optional.empty() ;
+        }
+
+        Parser dateParser = new Parser() ;
+
+        List<DateGroup> dates = dateParser.parse(rawString) ;
+
+        try {
+            Date date = dates.get(0).getDates().get(0) ;
+
+            return Optional.of(LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault())) ;
+
+        } catch (IndexOutOfBoundsException e) {
+            return Optional.empty() ;
+        }
+    }
+
+    public static String parseLocalDateTimeIntoString (LocalDateTime datetime) {
+        assert datetime != null ;
+
+        return datetime.format(FORMATTER) ;
+    }
+
+
+    private static Optional<Pair<LocalDateTime, LocalDateTime>> determineStartAndEndDateTime(Optional<LocalDateTime> startDate, Optional<LocalDateTime> endDate) {
+
+        LocalDateTime computedStartDate ;
+        LocalDateTime computedEndDate ;
+
+        if (!startDate.isPresent() && !endDate.isPresent()) {
+            computedStartDate = DateUtil.NOW ;
+            computedEndDate = DateUtil.END_OF_TODAY ;
+
             return Optional.of(new Pair<LocalDateTime, LocalDateTime> (computedStartDate, computedEndDate)) ;
         }
-	    
-	    if (startDate.isPresent() && endDate.isPresent()) {
-	       
-	        if (endDate.get().isBefore(startDate.get()) && isDateComponentSameAsNow(endDate.get())) {
-	            computedStartDate = startDate.get() ;
-	            
-	            int seconds = endDate.get().getSecond() ;
-	            int minutes = endDate.get().getMinute() ;
-	            int hours = endDate.get().getHour() ;
-	            
-	            computedEndDate = startDate.get().withHour(hours).withMinute(minutes).withSecond(seconds) ;
-	            
-	            return Optional.of(new Pair<LocalDateTime, LocalDateTime> (computedStartDate, computedEndDate)) ;
-	        }
-	    }
-	    
+
+        if (!startDate.isPresent() && endDate.isPresent()) {
+            computedStartDate = DateUtil.NOW ;
+            computedEndDate = endDate.get() ;
+
+            return Optional.of(new Pair<LocalDateTime, LocalDateTime> (computedStartDate, computedEndDate)) ;
+        }
+
+        if (startDate.isPresent() && !endDate.isPresent()) {
+            computedStartDate = startDate.get() ;
+            computedEndDate = startDate.get().withHour(23).withMinute(59) ;
+
+            return Optional.of(new Pair<LocalDateTime, LocalDateTime> (computedStartDate, computedEndDate)) ;
+        }
+
+        if (startDate.isPresent() && endDate.isPresent()) {
+
+            if (endDate.get().isBefore(startDate.get()) && isDateComponentSameAsNow(endDate.get())) {
+                computedStartDate = startDate.get() ;
+
+                int seconds = endDate.get().getSecond() ;
+                int minutes = endDate.get().getMinute() ;
+                int hours = endDate.get().getHour() ;
+
+                computedEndDate = startDate.get().withHour(hours).withMinute(minutes).withSecond(seconds) ;
+
+                return Optional.of(new Pair<LocalDateTime, LocalDateTime> (computedStartDate, computedEndDate)) ;
+            }
+        }
+
         return Optional.empty() ;
-	    
-	}
-	
-	
-	/**
+
+    }
+
+
+    /**
      * Given one and start and end date, the function will return a corresponding start and end dates following these rules: <p>
      * 
      * - If startdate and enddate are both empty, this function will return starting date of now and enddate of 2359 later today
@@ -111,18 +111,18 @@ public final class DateUtil {
      * @param endString string that will be parsed by natty
      * @return a LocalDateTime pair with the key being the starting datetime, and the value the ending datetime
      */
-	public static Optional<Pair<LocalDateTime, LocalDateTime>> determineStartAndEndDateTime(String startString, String endString) {
-	    return determineStartAndEndDateTime(parseStringIntoDateTime(startString), parseStringIntoDateTime(endString)) ;
-	}
-	
-	private static boolean isDateComponentSameAsNow (LocalDateTime dateTime) {
-	    LocalDate now = LocalDate.now() ;
-	    LocalDate givenDate = LocalDate.from(dateTime) ;
-	    
-	    if (now.equals(givenDate)) {
-	        return true ;
-	    }
-	    
-	    return false ;
-	}
+    public static Optional<Pair<LocalDateTime, LocalDateTime>> determineStartAndEndDateTime(String startString, String endString) {
+        return determineStartAndEndDateTime(parseStringIntoDateTime(startString), parseStringIntoDateTime(endString)) ;
+    }
+
+    private static boolean isDateComponentSameAsNow (LocalDateTime dateTime) {
+        LocalDate now = LocalDate.now() ;
+        LocalDate givenDate = LocalDate.from(dateTime) ;
+
+        if (now.equals(givenDate)) {
+            return true ;
+        }
+
+        return false ;
+    }
 }
