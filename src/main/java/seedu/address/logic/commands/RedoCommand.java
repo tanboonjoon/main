1643f408d8c1611ceb6a35d85d74c02c5aa20e7e
SAN SOK SAN <sansoksan@u.nus.edu>
2016-10-22 20:42:54 +0800

    Make these command undoable.

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -3,6 +3,18 @@
  */
 package seedu.address.logic.commands;
 
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
+import javafx.util.Pair;
+import seedu.address.commons.events.model.TaskForceCommandExecutedEvent;
+import seedu.address.model.task.ReadOnlyTask;
+import seedu.address.model.task.Task;
+import seedu.address.model.task.UniqueTaskList.DuplicateTaskException;
+import seedu.address.model.task.UniqueTaskList.TaskNotFoundException;
+
 public class RedoCommand extends Command {
 
     public static final String COMMAND_WORD = "redo";
@@ -11,14 +23,48 @@ public class RedoCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": redo the undo action.\n"
             + "Example: " + COMMAND_WORD;
-
+    private List<ReadOnlyTask> tasksAdded = Lists.newLinkedList();
+    private List<ReadOnlyTask> tasksDeleted = Lists.newLinkedList();
     @Override
     public CommandResult execute() {
-    
-        if(!model.restoreTaskForce()){
-            return new CommandResult(MESSAGE_NO_MORE_ACTION);
-        }
-        return new CommandResult(MESSAGE_SUCCESS);
-    }
+        TaskForceCommandExecutedEvent changes = (TaskForceCommandExecutedEvent) model.restoreChanges();
+        if(changes != null){
+            Command cmd = ((TaskForceCommandExecutedEvent) changes).commandInstance;
+            List<ReadOnlyTask> toDelete = cmd.getCommandChanges().getKey();
+            List<ReadOnlyTask> toAdd = cmd.getCommandChanges().getValue();
+            if(toDelete.isEmpty() && toAdd.isEmpty()){
+                return new CommandResult(MESSAGE_NO_MORE_ACTION);
+            }
+            for(ReadOnlyTask add: toAdd){
+                try {
+                    model.addTask((Task) add);
+                    tasksAdded.add(add);
 
+                } catch (DuplicateTaskException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            
+            for(ReadOnlyTask remove: toDelete){
+                try {
+                    model.deleteTask(remove);
+                    tasksDeleted.add(remove);
+
+                } catch (TaskNotFoundException e) {
+                    assert false: "task not found";
+                    e.printStackTrace();
+                }
+            }
+            
+            return new CommandResult(MESSAGE_SUCCESS);
+
+        }
+        return new CommandResult(MESSAGE_NO_MORE_ACTION);
+
+    }
+    @Override
+    public Pair<List<ReadOnlyTask>, List<ReadOnlyTask>> getCommandChanges() {
+        return new Pair<List<ReadOnlyTask>, List<ReadOnlyTask>>(ImmutableList.copyOf(tasksAdded), ImmutableList.copyOf(tasksDeleted)) ; 
+    }
 }
--- src/main/java/seedu/address/logic/parser/FreetimeCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FreetimeCommandParser.java
@@ -1,13 +1,11 @@
 package seedu.address.logic.parser;
 
 import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.FreetimeCommand;
 import seedu.address.logic.commands.IncorrectCommand;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import edu.emory.mathcs.backport.java.util.Arrays;
 import seedu.address.commons.exceptions.IncorrectCommandException;
 
 /*
@@ -71,7 +69,7 @@ public class FreetimeCommandParser extends CommandParser {
 
 	public boolean checkValidInt(String parsedArg) {
 		try {
-			int getInteger = Integer.parseInt(parsedArg);
+			Integer.parseInt(parsedArg);
 		} catch(NumberFormatException e) {
 			return false;
 		}
