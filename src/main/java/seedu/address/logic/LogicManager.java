41ad77eb797b4bebeed6deb3fe265cbfac9b6298
SAN SOK SAN <sansoksan@u.nus.edu>
2016-10-22 20:40:13 +0800

    revert rename of execute method in LogicManager.

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -30,7 +30,7 @@ public class LogicManager extends ComponentManager implements Logic {
     }
 
     @Override
-    public CommandResult invoke(String commandText) {
+    public CommandResult execute(String commandText) {
         logger.info("----------------[USER COMMAND][" + commandText + "]");
         Command command = parser.parseCommand(commandText);
         command.setData(model);
