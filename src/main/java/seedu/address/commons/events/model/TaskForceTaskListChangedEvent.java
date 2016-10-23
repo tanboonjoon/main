55badd1853bff5b60d615c66f9f87bd725afa7e1
MightyCupcakes <gohydj2003@gmail.com>
2016-10-23 23:42:38 +0800

    Changed the list change event to instead pass the Task object added, instead of the index

--- src/main/java/seedu/address/commons/events/model/TaskForceTaskListChangedEvent.java
+++ src/main/java/seedu/address/commons/events/model/TaskForceTaskListChangedEvent.java
@@ -1,18 +1,29 @@
 package seedu.address.commons.events.model;
 
+import java.util.List;
+
 import seedu.address.commons.events.BaseEvent;
+import seedu.address.model.task.ReadOnlyTask;
 
 public class TaskForceTaskListChangedEvent extends BaseEvent {
     
-    public final int targetIndex ;
+    public final List<ReadOnlyTask> tasks ;
     
-    public TaskForceTaskListChangedEvent (int index) {
-        this.targetIndex = index ;
+    public TaskForceTaskListChangedEvent (List<ReadOnlyTask> tasks) {
+        this.tasks = tasks; 
     }
     
     @Override
     public String toString() {
-        return "new index: " + targetIndex ;
+        StringBuilder sb = new StringBuilder () ;
+        
+        for (ReadOnlyTask task : tasks) {
+            sb.append(task.getName()) ;
+            sb.append(", ") ;
+        }
+        
+        sb.delete(sb.length() - 2, sb.length());
+        return "Names of tasks added: " + sb.toString() ;
     }
 
 }
--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -127,7 +127,7 @@ public class UiManager extends ComponentManager implements Ui {
     @Subscribe
     private void handleTaskListChangedEvent(TaskForceTaskListChangedEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        mainWindow.getTaskListPanel().scrollTo(event.targetIndex);
+        mainWindow.getTaskListPanel().scrollToTask(event.tasks.get(event.tasks.size() - 1));
     }
 
 }
