56a30ecd6e24bea42125bf0ba18afedd1e7985aa
tanboonjoon <tboonjoon@gmail.com>
2016-10-24 19:50:02 +0800

    more test case for freetime

--- src/test/java/guitests/FreetimeTest.java
+++ src/test/java/guitests/FreetimeTest.java
@@ -16,10 +16,33 @@ public class FreetimeTest extends TaskForceGuiTest{
 	private static final int AN_HOUR = 60;
 	private static final int EXACT_AN_HOUR = 00;
 	
+	private LocalDateTime yesterday;
+	private LocalDateTime now;
+	private LocalDateTime tomorrow;
+	
+	private DateTimeFormatter addCommandFormatter;
+	private DateTimeFormatter ongoingEventFormatter;
+	private DateTimeFormatter eventFormatter;
 	@Before 
 	public void clearList() {
 		commandBox.runCommand("clear");
 	}
+
+	
+	@Before
+	public void setUp() {
+
+
+		addCommandFormatter = DateTimeFormatter.ofPattern("MM-dd-yyy HHmm");
+		ongoingEventFormatter = DateTimeFormatter.ofPattern("dd/MM/yyy HHmm");
+		eventFormatter =  DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+		now = LocalDateTime.now();
+		yesterday = roundUpTime(now.minusDays(1));
+		tomorrow = roundUpTime(now.plusDays(1));
+	}
+		 
+	
 	@Test
 	public void invalidCommand() {
 		String invalidCommand = String.format(MESSAGE_INVALID_COMMAND_FORMAT , FreetimeCommand.MESSAGE_USAGE);
@@ -45,12 +68,10 @@ public class FreetimeTest extends TaskForceGuiTest{
 	
 	@Test
 	public void validCommandOneEvent() {
-		LocalDateTime now = LocalDateTime.now();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		commandBox.runCommand("add event st/today 3pm et/today 5pm");
 		StringBuilder sb = new StringBuilder();
 		commandBox.runCommand("freetime day/0");
-		sb.append(String.format(FreetimeCommand.DEFAULT_STARTING_MESSAGE, now.format(formatter)))
+		sb.append(String.format(FreetimeCommand.DEFAULT_STARTING_MESSAGE, now.format(eventFormatter)))
 		.append(String.format(FreetimeCommand.FIRST_EVENT_MESSAGE, "1500"))
 		.append(String.format(FreetimeCommand.LAST_EVENT_MESSAGE, "1700"));
 		assertResultMessage(sb.toString());
@@ -58,17 +79,13 @@ public class FreetimeTest extends TaskForceGuiTest{
 	
 	@Test
 	public void validCommandOneOngoingEvent() {
-		DateTimeFormatter addFormat = DateTimeFormatter.ofPattern("MM-dd-yyy HHmm");
-		LocalDateTime now = LocalDateTime.now();
-		LocalDateTime yestarday = roundUpTime(now.minusDays(1));
-		LocalDateTime tomorrow = roundUpTime(now.plusDays(1));
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		DateTimeFormatter ongoingFormat = DateTimeFormatter.ofPattern("dd/MM/yyy HHmm");
-		commandBox.runCommand("add event st/" + yestarday.format(addFormat) + " et/" + tomorrow.format(addFormat));
+		
+
+		commandBox.runCommand("add event st/" + yesterday.format(addCommandFormatter) + " et/" + tomorrow.format(addCommandFormatter));
 		StringBuilder sb = new StringBuilder();
 		commandBox.runCommand("freetime day/0");
-		sb.append(String.format(FreetimeCommand.DEFAULT_STARTING_MESSAGE, now.format(formatter)))
-		.append(String.format(FreetimeCommand.ONGOING_EVENT_MESSAGE, yestarday.format(ongoingFormat), tomorrow.format(ongoingFormat)));
+		sb.append(String.format(FreetimeCommand.DEFAULT_STARTING_MESSAGE, now.format(eventFormatter)))
+		.append(String.format(FreetimeCommand.ONGOING_EVENT_MESSAGE, yesterday.format(ongoingEventFormatter), tomorrow.format(ongoingEventFormatter)));
 		assertResultMessage(sb.toString());
 	}
 	
@@ -76,70 +93,69 @@ public class FreetimeTest extends TaskForceGuiTest{
 	
 	@Test
 	public void validCommandOneOngoingEndTodayEvent() {
-		LocalDateTime now = LocalDateTime.now();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		commandBox.runCommand("add event st/yesterday 3pm et/today 5pm");
 		StringBuilder sb = new StringBuilder();
 		commandBox.runCommand("freetime day/0");
-		sb.append(String.format(FreetimeCommand.DEFAULT_STARTING_MESSAGE, now.format(formatter)))
+		sb.append(String.format(FreetimeCommand.DEFAULT_STARTING_MESSAGE, now.format(eventFormatter)))
 		.append(String.format(FreetimeCommand.LAST_EVENT_MESSAGE, "1700"));
 		assertResultMessage(sb.toString());
 	}
 	
 	@Test
 	public void validCommandOneOngoingStartTodayEvent() {
-		LocalDateTime now = LocalDateTime.now();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
 		commandBox.runCommand("add event st/today 3pm et/tomorrow 5pm");
 		StringBuilder sb = new StringBuilder();
 		commandBox.runCommand("freetime day/0");
-		sb.append(String.format(FreetimeCommand.DEFAULT_STARTING_MESSAGE, now.format(formatter)))
+		sb.append(String.format(FreetimeCommand.DEFAULT_STARTING_MESSAGE, now.format(eventFormatter)))
 		.append(String.format(FreetimeCommand.FIRST_EVENT_MESSAGE, "1500"));
 		assertResultMessage(sb.toString());
 	}
-	
-	/*
+
 	@Test
-	public void valid_command_one_long_event() {
-		commandBox.runCommand("add event st/10-21-2016 1700 et/10-22-2016 2100");
+	public void validCommandMultipleEvent() {
+		commandBox.runCommand("add event st/today 3am et/today 12pm");
+		commandBox.runCommand("add event2 st/today 2:30am et/today 11am");
+		commandBox.runCommand("add event3 st/today 2pm et/today 5pm");
+		commandBox.runCommand("freetime day/0");
 		StringBuilder sb = new StringBuilder();
-		commandBox.runCommand("freetime day/1");
-		sb.append(String.format(FreetimeCommand.DEFAULT_STARTING_MESSAGE,"21/10/2016"))
-		.append(String.format(FreetimeCommand.FIRST_EVENT_MESSAGE, "1700"));
+		sb.append(String.format(FreetimeCommand.DEFAULT_STARTING_MESSAGE, now.format(eventFormatter)))
+		.append(String.format(FreetimeCommand.FIRST_EVENT_MESSAGE, "0230"))
+		.append(String.format(FreetimeCommand.BETWEEN_EVENT_MESSAGE, "1200", "1400"))
+		.append(String.format(FreetimeCommand.LAST_EVENT_MESSAGE, "1700"));
+		
 		assertResultMessage(sb.toString());
 	}
 	
 	@Test
-	public void valid_command_multiple_event() {
-		commandBox.runCommand("add event st/10-21-2016 1300 et/10-21-2016 1400");
-		commandBox.runCommand("add event st/10-21-2016 1330 et/10-21-2016 1340");
-		commandBox.runCommand("add event2 st/10-21-2016 1500 et/10-21-2016 1700");
-		commandBox.runCommand("add event3 st/10-21-2016 1520 et/10-21-2016 1730 ");
-		commandBox.runCommand("mark 4");
+	public void validCommandLongEventWithManyEvent() {
+		commandBox.runCommand("add event st/today 3am et/today 12pm");
+		commandBox.runCommand("add event2 st/today 2:30am et/today 11pm");
+		commandBox.runCommand("add event st/" + yesterday.format(addCommandFormatter) + " et/" + tomorrow.format(addCommandFormatter));
 		StringBuilder sb = new StringBuilder();
-		commandBox.runCommand("freetime day/1");
-		sb.append(String.format(FreetimeCommand.DEFAULT_STARTING_MESSAGE,"21/10/2016"))
-		.append(String.format(FreetimeCommand.FIRST_EVENT_MESSAGE, "1300"))
-		.append(String.format(FreetimeCommand.BETWEEN_EVENT_MESSAGE, "1330", "1500"))
-		.append(String.format(FreetimeCommand.LAST_EVENT_MESSAGE,  "1700"));
+		commandBox.runCommand("freetime day/0");
+		sb.append(String.format(FreetimeCommand.DEFAULT_STARTING_MESSAGE, now.format(eventFormatter)))
+		.append(String.format(FreetimeCommand.ONGOING_EVENT_MESSAGE, yesterday.format(ongoingEventFormatter), tomorrow.format(ongoingEventFormatter)));
 		assertResultMessage(sb.toString());
 		
 	}
 	
-	
-	@Test	
-	public void valid_command_mutiple_event_long() {
-		commandBox.runCommand("add event st/10-21-2016 1300 et/10-21-2016 1400");
-		commandBox.runCommand("add event2 st/10-21-2016 1500 et/10-22-2016 1700");
-		
+	@Test
+	public void validCommandMutipleEventEndLater() {
+		commandBox.runCommand("add event st/today 2:30am et/today 11am");
+		commandBox.runCommand("add event2 st/" + yesterday.format(addCommandFormatter) + " et/today 12pm");
+		commandBox.runCommand("add event3 st/today 5pm et/today 6pm");
+		commandBox.runCommand("freetime day/0");
 		StringBuilder sb = new StringBuilder();
-		commandBox.runCommand("freetime day/1");
-		sb.append(String.format(FreetimeCommand.DEFAULT_STARTING_MESSAGE,"21/10/2016"))
-		.append(String.format(FreetimeCommand.FIRST_EVENT_MESSAGE, "1300"))
-		.append(String.format(FreetimeCommand.BETWEEN_EVENT_MESSAGE, "1400", "1500"));
+		sb.append(String.format(FreetimeCommand.DEFAULT_STARTING_MESSAGE, now.format(eventFormatter)))
+		.append(String.format(FreetimeCommand.BETWEEN_EVENT_MESSAGE, "1200", "1700"))
+		.append(String.format(FreetimeCommand.LAST_EVENT_MESSAGE, "1800"));
 		assertResultMessage(sb.toString());
 	}
-	*/
+		
+		
+	
+
 	@After
 	public void clear() {
 		commandBox.runCommand("clear");
