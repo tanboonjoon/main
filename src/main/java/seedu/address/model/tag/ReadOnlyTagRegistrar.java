fee34dc1e4677d9f3c77b1681331f403a67b87a0
MightyCupcakes <gohydj2003@gmail.com>
2016-10-25 12:12:17 +0800

    Overhauled how tag works.
    Under this new system, all tag creation shall be deferred to a central tag registry. This registry will be responsible for all tag creation, edits and searching. In this new system all tags with similar names (case insensetive) will share the same tag object and any edits to the tag (name changing) will therefore reflect in all tasks that posesses that tag.

---
+++ src/main/java/seedu/address/model/tag/ReadOnlyTagRegistrar.java
@@ -0,0 +1,40 @@
+package seedu.address.model.tag;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+
+
+/**
+ * @@author A0135768R
+ * 
+ * A tag registry that disallows classes to reset the whole registry.
+ */
+public interface ReadOnlyTagRegistrar {
+    
+    public static final String MESSAGE_DUPLICATE_NAME = "The given tag name already exists!" ;
+    
+    /**
+     * Given a tag string to the registry, the registry will either return the Tag object with tag name
+     * matching the given string.
+     * <p>
+     * Additionally if register is set to true,
+     * if the tag is not currently in the registry, this create a new Tag object with the given
+     * string and returns it.
+     * 
+     * @param tagString     The tag name to be queried
+     * @param register      If true, this will register a new tag with the given name if not found; otherwise this will return null;
+     * @return      A tag object with the given tag name 
+     * @throws IllegalValueException if the given tag string is illegal and/or invalid
+     */
+    public Tag getTagFromString (String tagString, boolean register) throws IllegalValueException ;
+    
+    /**
+     * Given an existing tag, change its name to the given string
+     * 
+     * @param tag   The tag object whose name is to be modified
+     * @param newName   the name to change into
+     * 
+     * @throws IllegalValueException if the given name is invalid and/or already exists.
+     */
+    public void changeTagName(Tag tag, String newName) throws IllegalValueException ;
+
+}
