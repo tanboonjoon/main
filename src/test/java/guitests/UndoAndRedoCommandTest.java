8b328ac73dcce84f6642af11ce73b745e4abb9c5
Ning Yu <ningyu321@gmail.com>
2016-10-27 03:28:43 +0800

    added some tests for undo

--- src/test/java/guitests/UndoAndRedoCommandTest.java
+++ src/test/java/guitests/UndoAndRedoCommandTest.java
@@ -14,6 +14,7 @@ public class UndoAndRedoCommandTest extends TaskForceGuiTest {
     @Test
     public void undo(){
         
+    
         commandBox.runCommand("add event 1");
         commandBox.runCommand("add event 2");
         commandBox.runCommand("add event 3");
@@ -68,7 +69,52 @@ public class UndoAndRedoCommandTest extends TaskForceGuiTest {
         commandBox.runCommand("add test redo clear clear");
         commandBox.runCommand("redo");
         assertResultMessage(RedoCommand.MESSAGE_NO_MORE_ACTION);
-        //clear taskForce 
+        //clear taskForce      
+        commandBox.runCommand("clear");
+        
+        // @@author A0111277M
+        // testing undo upon other commands
+
+        commandBox.runCommand("add event");
+        
+        // can undo edits
+        commandBox.runCommand("edit 1 st/0000 et/2359");
+        commandBox.runCommand("undo");
+        assertResultMessage(UndoCommand.MESSAGE_SUCCESS);
+        
+        // can undo mark
+        commandBox.runCommand("mark 1");
+        commandBox.runCommand("undo");
+        assertResultMessage(UndoCommand.MESSAGE_SUCCESS);
+        
+        // can undo delete
+        commandBox.runCommand("delete 1");
+        commandBox.runCommand("undo");
+        assertResultMessage(UndoCommand.MESSAGE_SUCCESS);
+        
+        // can undo multiple delete
+        commandBox.runCommand("add another event");
+        commandBox.runCommand("delete 1, 2");
+        commandBox.runCommand("undo");
+        assertResultMessage(UndoCommand.MESSAGE_SUCCESS);
+        
+        // can undo block
+        commandBox.runCommand("redo");
+        commandBox.runCommand("block blocker st/1000 et/1100 st/1200 et/1300");
+        commandBox.runCommand("undo");
+        assertResultMessage(UndoCommand.MESSAGE_SUCCESS);
+        
+        // can undo edit block
+        commandBox.runCommand("redo");
+        commandBox.runCommand("edit 1 st/0000 et/2359");
+        commandBox.runCommand("undo");
+        assertResultMessage(UndoCommand.MESSAGE_SUCCESS);
+        
+        // can undo confirm
+        commandBox.runCommand("confirm 1 st/0000 et/2359");
+        commandBox.runCommand("undo");
+        assertResultMessage(UndoCommand.MESSAGE_SUCCESS);
+        
         commandBox.runCommand("clear");
     }
 
