b602ccc2da16c5bec8e7a8817da8c0f8e0bde513
MightyCupcakes <gohydj2003@gmail.com>
2016-10-31 14:52:41 +0800

    Some code refactors

--- src/main/java/seedu/address/commons/util/DateUtil.java
+++ src/main/java/seedu/address/commons/util/DateUtil.java
@@ -29,21 +29,23 @@ public final class DateUtil {
 
 
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm a");
+    
     public static final LocalDateTime END_OF_TODAY = parseStringIntoDateTime("today 2359").get() ;
     public static final LocalDateTime NOW = LocalDateTime.now() ;
-	public static final LocalDateTime MARKER_FOR_DELETE = LocalDateTime.MIN ;
-	public static final String STRING_FOR_DELETE = "-"; 
+    public static final LocalDateTime MARKER_FOR_DELETE = LocalDateTime.MIN ;
+    
+    public static final String STRING_FOR_DELETE = "-"; 
 
     public static Optional<LocalDateTime> parseStringIntoDateTime (String rawString) {
 
         if (rawString == null) {
             return Optional.empty() ;
         }
-        
-	    if (rawString.equals(STRING_FOR_DELETE)) {	
-	    	return Optional.of(MARKER_FOR_DELETE);
-	    }
-	    
+
+        if (rawString.equals(STRING_FOR_DELETE)) {	
+            return Optional.of(MARKER_FOR_DELETE);
+        }
+
 
         Parser dateParser = new Parser() ;
 
@@ -86,7 +88,7 @@ public final class DateUtil {
 
         model.searchTaskList(new PredicateExpression(new NameQualifier(Sets.newHashSet(days.toString()), NameQualifier.FILTER_BY_DAY, false)) );
 
-        PriorityQueue<Event> pq = new PriorityQueue<>(new EventComparator()) ;
+        PriorityQueue<Event> eventQueue = new PriorityQueue<>(new EventComparator()) ;
 
         for (ReadOnlyTask task : model.getSearchedTaskList()) {
 
@@ -94,16 +96,16 @@ public final class DateUtil {
                 continue ;
             }
 
-            pq.add((Event) task) ;
+            eventQueue.add((Event) task) ;
 
         }
 
         // Don't need to do anything if there is no events occuring on this time period
-        if (pq.isEmpty()) {
+        if (eventQueue.isEmpty()) {
             return Optional.empty() ;
         }
 
-        Event event = pq.poll() ;
+        Event event = eventQueue.poll() ;
 
         while (event != null && event.getEndDate().isAfter(startTime)) {
 
@@ -111,7 +113,7 @@ public final class DateUtil {
                 return Optional.of(event);
             }
 
-            event = pq.poll() ;
+            event = eventQueue.poll() ;
         }
 
         return Optional.empty() ;
@@ -127,7 +129,7 @@ public final class DateUtil {
         if (!startDate.isPresent() && !endDate.isPresent()) {
             return Optional.empty() ;
         }
-        
+
         if (!canBeEmpty && (!startDate.isPresent() || !endDate.isPresent())) {
             return Optional.empty() ;
         }
@@ -189,7 +191,7 @@ public final class DateUtil {
     public static Optional<Pair<LocalDateTime, LocalDateTime>> determineStartAndEndDateTime(String startString, String endString) {
         return determineStartAndEndDateTime(parseStringIntoDateTime(startString), parseStringIntoDateTime(endString), true) ;
     }
-    
+
     public static Optional<Pair<LocalDateTime, LocalDateTime>> determineStartAndEndDateTime(String startString, String endString, boolean allowEmptyValues) {
         return determineStartAndEndDateTime(parseStringIntoDateTime(startString), parseStringIntoDateTime(endString), allowEmptyValues) ;
     }
@@ -200,16 +202,16 @@ public final class DateUtil {
 
         return now.equals(givenDate) ;
     }
-    
+
     public static String getRelativeDateFromNow (LocalDateTime dateTime) {
         Long milis = ChronoUnit.MILLIS.between(NOW, dateTime) ;
-        
+
         return RelativeTimeConverter.toDuration(milis) ;
     }
-    
+
     public static long getTimeDifferenceFromNow (LocalDateTime dateTime, ChronoUnit units) {
         long diff = units.between(NOW, dateTime) ;
-        
+
         return diff ;
     }
 
@@ -235,7 +237,7 @@ public final class DateUtil {
     }
 
     private static class RelativeTimeConverter {
-        
+
         public static final ImmutableList<Long> TIMES = ImmutableList.of(
                 TimeUnit.DAYS.toMillis(365),
                 TimeUnit.DAYS.toMillis(30),
@@ -243,41 +245,41 @@ public final class DateUtil {
                 TimeUnit.HOURS.toMillis(1),
                 TimeUnit.MINUTES.toMillis(1),
                 TimeUnit.SECONDS.toMillis(1) );
-        
+
         public static final ImmutableList<String> TIME_STIRNGS = ImmutableList.of("year","month","day","hour","minute","second");
 
-        
+
         public static String toDuration(long miliseconds) {
-            
+
             long duration = Math.abs(miliseconds) ;
 
             StringBuffer sb = new StringBuffer();
-            
+
             for(int i = 0; i < TIMES.size(); i ++) {
                 Long current = TIMES.get(i);
                 long temp = duration/current;
-                
+
                 if(temp>0) {
                     sb.append(temp) ;
                     sb.append(" ") ;
                     sb.append( TIME_STIRNGS.get(i) ) ;
                     sb.append(temp > 1 ? "s" : "") ;
-                    
+
                     break;
                 }
             }
-            
+
             if( "".equals(sb.toString()) ) {
                 return "0 second ago";
             } 
-            
+
             if (miliseconds > 0) {
                 sb.append(" later") ;
-            
+
             } else {
                 sb.append(" ago") ;
             }
-            
+
             return sb.toString();
         }
     }
