1bafeada0862caf11042ed0421030f73f3fabad0
tanboonjoon <tboonjoon@gmail.com>
2016-11-03 17:37:59 +0800

    refractor findCommand test

--- src/test/java/guitests/FindCommandTest.java
+++ src/test/java/guitests/FindCommandTest.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.junit.After;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,14 +20,143 @@ import seedu.address.testutil.TestTask;
 
 //@@author A0139942W
 public class FindCommandTest extends TaskForceGuiTest {
+    private static final int MONDAY = 1;
+    private static final int SUNDAY = 7;
 
     @Before
     public void setUp() {
         commandBox.runCommand("clear");
+        commandBox.runCommand("add this is a task");
+        commandBox.runCommand("add this is a task with description d/this is a description");
+        commandBox.runCommand("add this is a task with tag t/important");
+        commandBox.runCommand("add todayEvent st today 9pm et/today 9pm");
+        commandBox.runCommand("add todayDeadline et/today 9pm");
+        commandBox.runCommand("add lastWeekEvent st/last week 2pm et/last week 4pm");
+        commandBox.runCommand("add lastWeekDeadline et/last week 2pm");
+    }
+    
+    @Test
+    public void findCommand_findByName_found() {
+        commandBox.runCommand("find name/event");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 2));
+        assertListSize(2);  
+    }
+    
+    @Test
+    public void findCommand_findByName_notFound() {
+        commandBox.runCommand("find name/nonExistentTask");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 0));
+        assertListSize(0);  
+    }
+    
+    
+    @Test
+    public void findCommand_findByDesc_found() {
+        commandBox.runCommand("find desc/this");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 1));
+        assertListSize(1);   
+    }
+    
+    public void findCommand_findByDesc_notFound() {
+        commandBox.runCommand("find desc/cannotBeFound");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 0));
+        assertListSize(0);   
+    }
+    
+    @Test
+    public void findCommand_findByTag_found() {
+        commandBox.runCommand("find tag/imp");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 1));
+        assertListSize(1);  
+    }
+    
+    @Test
+    public void findCommand_findByTag_notFound() {
+        commandBox.runCommand("find tag/friend");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 0));
+        assertListSize(0);  
+    }
+    
+    @Test
+    public void findCommand_includeMarkFilter_found() {
+        commandBox.runCommand("mark 1");
+        commandBox.runCommand("find name/this ");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 2));
+        assertListSize(2);  
+        
+        commandBox.runCommand("find name/this mark/true ");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 3));
+        assertListSize(3);  
+    }
+
+    
+    /*
+     * for find by day/week, floating test will always be displayed. Since we have 3 floating task,
+     * the min size of the list using find day/ and find week/ should always be 3
+     */
+    @Test
+    public void findCommand_findByDay_found() {
+        commandBox.runCommand("find day/0");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 5));
+        assertListSize(5);  
+    }
+    
+    @Test
+    public void findCommand_findByDay_notFound() {
+        commandBox.runCommand("find day/-10");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 3));
+        assertListSize(3);  
+    }
+    
+    @Test
+    public void findCommand_findByWeek_found() {
+        addMoreTask();
+        commandBox.runCommand("find week/0");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 9));
+        assertListSize(9);  
+        
+        commandBox.runCommand("find week/-1");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 5));
+        assertListSize(5);  
+    }
+    
+    @Test
+    public void findCommand_findByWeek_notFound() {
+        addMoreTask();
+        commandBox.runCommand("find week/5");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 3));
+        assertListSize(3);  
+    }
+    
+    @Test
+    public void findCommand_findByType_all_found() {
+        commandBox.runCommand("find type/all");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 7));
+        assertListSize(7);  
+    }
+    
+    @Test
+    public void findCommand_findByType_marked_found() {
+        commandBox.runCommand("mark 1");
+        commandBox.runCommand("mark 3");
+        commandBox.runCommand("find type/mark");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 2));
+        assertListSize(2); 
+    }
+    
+    @Test
+    public void findCommand_findByType_overdue_found() {
+        commandBox.runCommand("find type/overdue");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 1));
+        assertListSize(1); 
+        commandBox.runCommand("mark 1");
+        commandBox.runCommand("find type/overdue");
+        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 0));
+        assertListSize(0);
     }
 
     @Test
-    public void findInvalidCommandFail() {
+    public void findCommand_invalidCommand_showInvalidMessage() {
         commandBox.runCommand("find day/123 sdf");
         assertResultMessage(FindCommand.INVALID_FIND_DATE_MESSAGE);
         commandBox.runCommand("find week/thisIsNotNumber");
@@ -41,87 +170,45 @@ public class FindCommandTest extends TaskForceGuiTest {
         assertResultMessage(FindCommand.INVALID_FIND_TYPE_MESSAGE);
 
     }
-
-    @Test
-    public void validCommandFindName() {
-        commandBox.runCommand("add this is a task");
-        commandBox.runCommand("add this is a event st today 9pm et/today 9pm");
-        commandBox.runCommand("add this is a deadline et/today 9pm");
-        commandBox.runCommand("find name/this is");
-        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 3));
-
+    
+    private void addMoreTask() {
+        LocalDateTime now = LocalDateTime.now();
+        int dayOfTheWeek = now.getDayOfWeek().getValue();
+        
+        if (dayOfTheWeek == MONDAY) {
+            commandBox.runCommand("add tomorrowDeadline et/tomorrow 6pm");
+            commandBox.runCommand("add tomorrowEvent st/tomorrow 2pm et/tomorrow 6pm");
+            commandBox.runCommand("add theDayAfterDeadline et/2 day later 6pm");
+            commandBox.runCommand("add theDayAfterEvent st/2 day later 2pm et/2 day later 6pm");
+            return;
+        }
+        
+        if (dayOfTheWeek == SUNDAY) {
+            commandBox.runCommand("add 2dayAgoDeadline et/2 day ago 6pm");
+            commandBox.runCommand("add 2dayAgoEvent st/2 day ago 2pm et/2 day ago 6pm");
+            commandBox.runCommand("add yesterdayDeadline et/ yesterday 6pm");
+            commandBox.runCommand("add yesterdayEvent st/yesterday 2pm et/yesterday 6pm");
+            return;
+        }
+        
+        commandBox.runCommand("add tomorrowDeadline et/tomorrow 6pm");
+        commandBox.runCommand("add tomorrowEvent st/tomorrow 2pm et/tomorrow 6pm");
+        commandBox.runCommand("add yesterdayDeadline et/ yesterday 6pm");
+        commandBox.runCommand("add yesterdayEvent st/yesterday 2pm et/yesterday 6pm");
+        return;      
     }
 
-    @Test
-    public void validCommandFindTag() {
-        commandBox.runCommand("add task1 this has a tag t/important");
-        commandBox.runCommand("add task2 this has no tag ");
-        commandBox.runCommand("add task3 this has no tag 2 ");
-        commandBox.runCommand("find tag/imp");
-        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 1));
-    }
-
-    @Test
-    public void validCommandFindDesc() {
-        commandBox.runCommand("add a task d/this has description");
-        commandBox.runCommand("find desc/this has description");
-        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 1));
-    }
-
-    @Test
-    public void validCommandFindMarkedTasked() {
-        commandBox.runCommand("add a task that is marked");
-        commandBox.runCommand("mark 1");
-        commandBox.runCommand("find name/task");
-        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 0));
-        commandBox.runCommand("find name/task mark/true");
-        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 1));
-    }
-
-    @Test
-    public void validCommandFindDay() {
-        commandBox.runCommand("add a event st/today 12pm et/today 5pm");
-        commandBox.runCommand("add a task ");
-        commandBox.runCommand("add a deadline et/today 2pm");
-        commandBox.runCommand("add notRelevantEvent st/last week 2pm et/last week 4pm");
-        commandBox.runCommand("find day/0");
-        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 3));
-    }
-
-    @Test
-    public void validCommandFindWeek() {
-        commandBox.runCommand("add nextWeekEvent st/next week 2pm et/9 day later 5pm");
-        commandBox.runCommand("add nextWeekDeadline et/next week 5pm ");
-        commandBox.runCommand("add this is a normal task");
-        commandBox.runCommand("add Not Relevant event st/today 2pm et/today 6pm");
-        commandBox.runCommand("find week/1");
-        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 3));
-    }
-
-    @Test
-    public void validCommandFindType() {
-        commandBox.runCommand("add overdueDeadline et/last week 2pm");
-        commandBox.runCommand("add floatingTask");
-        commandBox.runCommand("add LastmarkedTask et/today 5pm");
-        commandBox.runCommand("mark 2");
-        commandBox.runCommand("find type/all");
-        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 3));
-        commandBox.runCommand("find type/mark");
-        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 1));
-        commandBox.runCommand("find type/overdue");
-        assertResultMessage(String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, 1));
-    }
 
 // @@author A0135768R
 
     @Test
-    public void findEmptyList() {
+    public void findCommand_emptyList_noResult() {
         commandBox.runCommand("clear");
         assertFindResult("find name/Jean"); // no results
     }
 
     @Test
-    public void findMinimalValidCommand_pass() {
+    public void findCommand_validCommand_pass() {
 
         commandBox.runCommand("clear");
 
@@ -187,8 +274,4 @@ public class FindCommandTest extends TaskForceGuiTest {
         return list;
     }
 
-    @After
-    public void clear() {
-        commandBox.runCommand("clear");
-    }
 }
